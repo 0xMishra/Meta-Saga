73017e22bf6d51c2ba7eec3a4fb91aceaed335a9
kundan mishra <mishrakundan073@gmail.com>
2022-03-18 20:34:52 +0530

    packages update

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -136,7 +136,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "b0fe2b5dfe0b3736"; }
+/******/ 		__webpack_require__.h = function() { return "eed5185da1182ec3"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
